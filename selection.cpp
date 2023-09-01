5ab1c656a11c81f4ecd01626ac971cc6bfeb170f
kolinco mondol akash <92842628+luchifer2@users.noreply.github.com>
2023-09-02 00:24:36 +0600

    Add files via upload

---
+++ selection.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n, int &swapCount) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex != i) {
+            swap(arr[i], arr[minIndex]);
+            swapCount++;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int swapCount = 0;
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr, n, swapCount);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Number of swaps: " << swapCount << endl;
+
+    return 0;
+}
