ae22c99de093011ae3efc59d51044e59f1d0b2f6
kolinco mondol akash <92842628+luchifer2@users.noreply.github.com>
2023-09-02 00:23:37 +0600

    Add files via upload

---
+++ bubble.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n, int &swapCount) {
+    bool swapped;
+    for (int i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapCount++;
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, the array is already sorted
+        if (!swapped)
+            break;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int swapCount = 0;
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, n, swapCount);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Number of swaps: " << swapCount << endl;
+
+    return 0;
+}
